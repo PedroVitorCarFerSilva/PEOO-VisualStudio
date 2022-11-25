5135c335166ed003fe88b3c8fb1dfb665307944f
Pedro Vitor Carvalho Ferreira da Silva <carvalho.ferreira@academico.ifrn.edu.br>
2022-11-25 16:11:30 -0300

    ghuhuhuhuhuhuhuh

---
+++ pasta/pequenas empresas/cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pequenas_empresas
+{
+    class cliente
+    {
+        private string nome;
+        private double limite;
+        public cliente(string n, double l)
+        {
+            nome = n;
+            if (l > 0) limite = l;
+        }
+        public override string ToString()
+        {
+            return $"nome do cliente: {nome} - limite: {limite}";
+        }
+    }
+}
---
+++ pasta/pequenas empresas/empresa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pequenas_empresas
+{
+    class empresa
+    {
+        private string nome;
+        private cliente[] c = new cliente[10];
+        public empresa(string n)
+        {
+            nome = n;
+        }
+        public override string ToString()
+        {
+            return $"nome da empresa: {nome}";
+        }
+    }
+}
